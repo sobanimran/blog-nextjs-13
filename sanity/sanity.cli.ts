b07105ea005ff424221938c0a5a2d0785ed72990
sobanimran <soubansheikh163@gmail.com>
2023-11-25 10:53:34 +0500

    first commit

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'chm5i8o0',
+    dataset: 'production'
+  }
+})
---
+++ src/app/lib/interface.ts
@@ -0,0 +1,14 @@
+
+
+export interface Post  {
+  title :string,
+  overview:string;
+  content :any;
+    _id : string;
+    slug:{
+        current:string;
+    };
+    _createdAt:string;
+  
+}
+
